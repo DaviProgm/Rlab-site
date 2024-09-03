80d6fddd7ade8b7a15491d71be85d6f9e48a982b
Samuel Lima <105258182+Samuelrlm@users.noreply.github.com>
2024-09-03 15:54:12 -0300

    add script

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log('Hello, world!');
+const name = document.getElementById('name')
+const number = document.getElementById('number');
+const time = document.getElementById('time');
+const date = document.getElementById('dt');
+const text = document.getElementById('text');
+const button = document.getElementById('btn-send');
+
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const text = `Olá, meu nome é ${name.value}, gostaria de agendar um horário para ${time.value} no dia ${date.value}.`;
+    const phone = '5585991261036';
+
+    const wpp_url = `https://api.whatsapp.com/send?phone=${phone}&text=${encodeURIComponent(text)}`;
+
+    window.open(wpp_url, '_blank');
+});
