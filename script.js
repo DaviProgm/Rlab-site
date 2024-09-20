4a75fc94f64d626c08ae7004c810d8cf706c6272
Davi Monteiro <d8119514@gmail.com>
2024-09-20 12:08:06 -0300

    commit5

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-console.log('Hello, world!');
-const name = document.getElementById('name')
-const number = document.getElementById('number');
-const time = document.getElementById('time');
-const date = document.getElementById('dt');
-const text = document.getElementById('text');
-const button = document.getElementById('btn-send');
-
-button.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    const text = `Olá, meu nome é ${name.value}, gostaria de agendar um horário para ${time.value} no dia ${date.value} Estaria disponivel? .` ;
-    const phone = '558585603558';
-
-    const wpp_url = `https://api.whatsapp.com/send?phone=${phone}&text=${encodeURIComponent(text)}`;
-
-    window.open(wpp_url, '_blank');
-});
+console.log("Hello, world!");const name=document.getElementById("name"),number=document.getElementById("number"),time=document.getElementById("time"),date=document.getElementById("dt"),text=document.getElementById("text"),button=document.getElementById("btn-send");button.addEventListener("click",a=>{a.preventDefault();const b=`Olá, meu nome é ${name.value}, gostaria de agendar um horário para ${time.value} no dia ${date.value} Estaria disponivel? .`,c=`https://api.whatsapp.com/send?phone=${"558585603558"}&text=${encodeURIComponent(b)}`;window.open(c,"_blank")});
